1d1e8e254431e387137d1e51be86e2ec50636986
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-29 13:01:06 +0100

    Header component added

--- src/App.js
+++ src/App.js
@@ -1,29 +1,10 @@
+import Header from './component/header';
+
 function App() {
   return (
-    <nav className={`navbar navbar-expand-lg  `}>
-      <div className="container-fluid mb-4">
-        <h2>ClearLink</h2>
-        <button
-          className={`navbar-toggler `}
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse ms-md-auto" id="navbarNav">
-          <ul className="navbar-nav ms-md-auto mb-2 mb-lg-0">
-            <li className={`nav-item`}>Products </li>
-            <li className="nav-item ">olutions </li>
-            <li className="nav-item ">esources </li>
-            <li className="nav-item ">Pricing </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <div className="container">
+      <Header />
+    </div>
   );
 }
 
