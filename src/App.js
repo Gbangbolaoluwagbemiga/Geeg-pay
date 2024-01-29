d617c1ab45c799d09fec4faa7fa503d71d54cbbf
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-29 12:37:22 +0100

    Testing

---
+++ src/App.js
@@ -0,0 +1,31 @@
+<nav className={`navbar navbar-expand-lg  ${styles.nav}`}>
+  <div className="container-fluid mb-4">
+    <Logo />
+    <button
+      className={`navbar-toggler ${styles.hamburger}`}
+      type="button"
+      data-bs-toggle="collapse"
+      data-bs-target="#navbarNav"
+      aria-controls="navbarNav"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse ms-md-auto" id="navbarNav">
+      <ul className="navbar-nav ms-md-auto mb-2 mb-lg-0">
+        <li className={`nav-item ${styles.pricing}`}>
+          <NavLink to={'/pricing'}>Pricing</NavLink>
+        </li>
+        <li className="nav-item ">
+          <NavLink to={'/product'}>Product</NavLink>
+        </li>
+        <li className="nav-item ">
+          <NavLink to={'/login'} className={styles.ctaLink}>
+            Sign in
+          </NavLink>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>;
