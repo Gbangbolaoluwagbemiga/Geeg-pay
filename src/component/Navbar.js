7da3524acf43d10394f8deba8969532671dd88cd
Gbangbolaoluwagbemiga <oluwagbemigagbangbola@gmail.com>
2024-01-29 15:47:51 +0100

    Component completed

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,10 +1,12 @@
+import arrDown from '../image/icon/chevron-down.svg';
+
 function Navbar() {
   return (
-    <nav className={`navbar navbar-expand-lg  `}>
+    <nav className={`navbar navbar-expand-lg mt-3 `}>
       <div className="container-fluid mb-4">
-        <h5>ClearLink</h5>
+        <h6 className="me">ClearLink</h6>
         <button
-          className={`navbar-toggler `}
+          className={`navbar-toggler hamburger `}
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
@@ -15,12 +17,25 @@ function Navbar() {
           <span className="navbar-toggler-icon"></span>
         </button>
         <div className="collapse navbar-collapse ms-md-auto" id="navbarNav">
-          <ul className="navbar-nav ms-md-auto mb-2 mb-lg-0">
-            <li className={`nav-item`}>Products </li>
-            <li className="nav-item ">olutions </li>
-            <li className="nav-item ">esources </li>
-            <li className="nav-item ">Pricing </li>
+          <ul className="navbar-nav mx-auto ">
+            <li className="nav-item me-md-4">
+              Products <img src={arrDown} alt="arrow-down" />{' '}
+            </li>
+            <li className="nav-item me-md-4">
+              Solutions <img src={arrDown} alt="arrow-down" />{' '}
+            </li>
+            <li className="nav-item me-md-4">
+              Resources <img src={arrDown} alt="arrow-down" />{' '}
+            </li>
+            <li className="nav-item me-md-4">
+              Pricing <img src={arrDown} alt="arrow-down" />{' '}
+            </li>
           </ul>
+
+          <div className="sign--up d-md-flex justify-content-between pt-3">
+            <p className=" me-md-3 talk-sales">Talk to sales</p>
+            <p className=" me-md-3 free-sign">sign up for free</p>
+          </div>
         </div>
       </div>
     </nav>
